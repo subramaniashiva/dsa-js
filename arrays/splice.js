8875eeaa26290ff1b4325819095b0de9191912dc
subramaniashiva <vengaishiva@gmail.com>
2015-07-01 23:50:43 +0530

    Array Splice Completed

--- arrays/splice.js
+++ arrays/splice.js
@@ -12,27 +12,46 @@
 Array.prototype.mySplice = function() {
     var resultArray = [];
     var begin = arguments[0],
-        numOfElems;
+        numOfElems, i, j;
+    // Assign the value of beginning index from which the array will be spliced
     if (typeof begin === 'undefined') {
         begin = 0;
     } else if (begin < 0) {
         begin = this.length + begin;
+        // If the value is still a negative value, 
+        // then the user given index falls outside the length of array.
+        // So set it to zero
+        begin = Math.max(begin, 0);
     }
-    if (arguments[1]) {
+    // If the number of elements to be spliced is not given, assume its till the
+    // end of the array
+    if (typeof arguments[1] !== 'undefined' || typeof arguments[1] !== 'null') {
         numOfElems = arguments[1];
+    } else {
+        numOfElems = this.length - begin;
     }
+    // If there are elements to be removed, remove it first
+    // And change the length of array
     if (numOfElems > 0) {
-        for (var i = begin, j = 0;
+        for (i = begin, j = 0;
             (j < numOfElems && i < this.length); i++, j++) {
             resultArray.push(this[i]);
             this[i] = this[i + numOfElems];
         }
         this.length -= numOfElems;
     }
+    // If there are elements to be added, add it to the element
+    var insertElems = Array.prototype.slice.call(arguments, 2);
+    if (insertElems.length) {
+        var tempArray = this.slice(begin);
+        this.length -= tempArray.length;
+        Array.prototype.push.apply(this, insertElems);
+        Array.prototype.push.apply(this, tempArray);
+    }
     return resultArray;
 }
 var myArray = [1, 2, 3, 4, 5, 6, 7];
-var newArray = myArray.mySplice(-6, 3);
-//var newArray = myArray.splice(-6, 3);
+var newArray = myArray.mySplice(3, 0, 12, 13, 14, 15, 16);
+//var newArray = myArray.splice(3, 0, 12, 13, 14, 15, 16);
 console.log('Values in Array ', myArray);
 console.log('Sliced Array ', newArray);
