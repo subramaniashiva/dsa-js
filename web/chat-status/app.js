e05e6d94ec07567ad236f091396fd4257c64bd3a
subramaniashiva <vengaishiva@gmail.com>
2015-08-21 19:10:49 +0530

    Working Prototype

--- web/chat-status/app.js
+++ web/chat-status/app.js
@@ -3,11 +3,16 @@
   var chatBox = document.getElementById('chat'),
       status = document.getElementById('status');
 
-  
+  var interval;
 
   chatBox.addEventListener('keyup', function(){
     if(chatBox.value) {
       status.innerHTML = 'User is typing';
+      clearTimeout(interval);
+      interval = setTimeout(function(){
+        console.log('called');
+        status.innerHTML = 'User has stopped typing';
+      }, 2000);
     }
     else {
       status.innerHTML = 'User has stopped typing';
