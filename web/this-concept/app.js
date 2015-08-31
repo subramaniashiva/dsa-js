f46ad8c96a095acdc0f8a5f976b4156a373d392f
subramaniashiva <vengaishiva@gmail.com>
2015-08-31 22:47:43 +0530

    Updated this concept

--- web/this-concept/app.js
+++ web/this-concept/app.js
@@ -11,30 +11,41 @@ function Person(name, age) {
     return 'Name is ' + this.name + '. Age is ' + this.age;
   }
 }
-
+// When initializing an object with 'new' keyword, 'this' inside the constructor
+// refers to the newly created object
 var personObj = new Person('Siva', 20);
-console.log(personObj.toString());
-
+console.log('Calling the method from an object created using constructor: ', personObj.toString());
+// If you miss the 'new' keyword while initializing an object, 'this' inside the constructor
+// refers to the scope where the new object is created.
+// In our case it polluts the global name space
 var faultyObj = Person('John', 30);
 console.log(faultyObj);
-console.log(this.name);
+console.log('Faulty Object polluting global namespace: ', this.name);
 
 var obj = {
-  name: 'Obj Name',
+  address: 'default address',
   print: function() {
-    console.log(this.name);
+    console.log(this.address);
   }
 };
-
+// When you call the method using an object, 'this' inside the method
+// refers to the object
+console.log('Calling an object\'s method: ');
 obj.print();
 
 var parentObj = {
   childObj: {
-    name: 'ChildObj',
+    age: 'ChildObj',
     print: function() {
-      console.log(this.name);
+      console.log(this.address);
     }
   }
 }
-
-parentObj.childObj.print();
+console.log('Calling a nested object\'s method: ');
+parentObj.childObj.address();
+// When you store the reference to the function in a variable, and call the variable later
+// 'this' in the emthod refers to the scope of the variable.
+// In our case, it refers to the global name space
+var referenceFunction = obj.address;
+console.log('Calling a reference function: ');
+referenceFunction();
