cc35611010b6627a6ab9c69e92ad9224b54596e2
subramaniashiva <vengaishiva@gmail.com>
2015-09-20 21:53:15 +0530

    Updated random number file

--- randomNumberGen.js
+++ randomNumberGen.js
@@ -4,9 +4,23 @@
   Generate random number between the given two numbers
 */
 function getRandomNumber(lower, upper, includeBounds) {
-	var random;
+	var random, temp;
 	if(arguments.length > 1) {
-		random = (Math.random() * (upper - lower)) + lower;
+    // convert upper and lower to integers
+    lower = parseInt(lower, 10);
+    upper = parseInt(upper, 10);
+    //  If the upper and lower are not correct
+    // swap them
+		if(lower > upper) {
+      temp = lower;
+      lower = upper;
+      upper = temp;
+    }
+    if(includeBounds) {
+		  random = (Math.random() * (upper - lower + 1)) + lower;
+    } else {
+      random = (Math.random() * (upper - lower)) + lower;
+    }
 		random = parseInt(random, 10);
 	} else {
 		random = Math.random();
@@ -14,4 +28,4 @@ function getRandomNumber(lower, upper, includeBounds) {
 	return random;
 }
 
-console.log(getRandomNumber(3, 5));
+console.log(getRandomNumber(3, 5, false));
