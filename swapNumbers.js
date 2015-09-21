3b30e7d4d01cf55fad5d7aa72ad2357135ce41ff
subramaniashiva <vengaishiva@gmail.com>
2015-09-21 20:44:05 +0530

    Added swapping values file

---
+++ swapNumbers.js
@@ -0,0 +1,20 @@
+'use strict';
+/*
+  Problem Statement
+  The very famous and age old puzzle :)
+  Swap two numbers without using temp variable
+*/
+function swapNumbers(numbersArray) {
+	if(numbersArray && numbersArray.length === 2) {
+		numbersArray[0] = 2*(numbersArray[1] - numbersArray[0]) + numbersArray[0];
+		numbersArray[1] = numbersArray[1] - (numbersArray[0] - numbersArray[1]);
+		numbersArray[0] = ((numbersArray[0] - numbersArray[1])/2) + numbersArray[1]
+		return numbersArray;
+	} else {
+		return numbersArray;
+	}
+}
+var a = [7, 0];
+swapNumbers(a);
+// Arrays are passed by reference :)
+console.log(a);
