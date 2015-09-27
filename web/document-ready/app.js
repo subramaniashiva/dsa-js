45b21610da4f45a659c624dc95d0142e043df3fb
subramaniashiva <vengaishiva@gmail.com>
2015-09-27 20:02:56 +0530

    Added DOM Ready file

---
+++ web/document-ready/app.js
@@ -0,0 +1,24 @@
+'use strict';
+// DOM ready function
+function domReady(callback, context) {
+	var args = Array.prototype.slice.call(arguments, 2);
+	if(document.addEventListener) {
+		document.addEventListener('DOMContentLoaded', function() {
+			callback.apply(context, args);
+		});
+	} else if(document.attachEvent) {
+		document.attachEvent('onreadystatechange', function() {
+			callback.apply(context, args);
+		});
+	}
+}
+var a =1, b =0, c = 3;
+domReady(function(d, e, f) {
+	console.log(d, e, f);
+}, undefined, a, b, c);
+// This can also be used
+/*document.addEventListener('readystatechange', function() {
+	var state = document.readyState;
+	if(state === 'interactive')
+		console.log('Ready State Change');
+})*/
