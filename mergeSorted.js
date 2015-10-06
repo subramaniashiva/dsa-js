5c601f48dac2f67ec2159b4db8a95e81d3506292
subramaniashiva <vengaishiva@gmail.com>
2015-10-06 22:22:00 +0530

    Cmpleted merging sorted array problem

--- mergeSorted.js
+++ mergeSorted.js
@@ -5,7 +5,31 @@
  */
 
  function merge(array1, array2) {
- 	var merged = [], j = 0;
- 	
+ 	var merged = [];
+ 	for(var i = 0, j = 0; i < array1.length;) {
+ 		if(i === array1.length || j === array2.length) {
+ 			break;
+ 		}
+ 		if(array1[i] < array2[j]) {
+ 			merged.push(array1[i]);
+ 			i++;
+ 		} else {
+ 			merged.push(array2[j]);
+ 			j++;
+ 		}
+ 	}
+ 	if(j <= array2.length) {
+ 		[].push.apply(merged, array2.splice(j));
+ 	}
+ 	if(i <= array1.length) {
+ 		[].push.apply(merged, array1.splice(i));
+ 	}
  	return merged;
  }
+
+ var array1 = [2, 4, 6, 8];
+ var array2 = [1, 3, 9, 11];
+
+ var merged = merge(array1, array2);
+ console.log(merged);
+
