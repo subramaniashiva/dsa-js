077dc669a332982e932bc1c4cb2e1dfebb12fa86
subramaniashiva <vengaishiva@gmail.com>
2015-10-07 23:30:44 +0530

    Completed remove duplicate chars form string file

--- removeDuplicateString.js
+++ removeDuplicateString.js
@@ -4,12 +4,23 @@
   Remove Duplicate Characters in String
 */
 function removeDuplicateChars(string) {
+	var result, matched = [];
 	if(string.length > 1) {
-
+		result = '';
+		for(var i = 0; i < string.length; i++) {
+			if(matched.indexOf(i) === -1) {
+				for(var j = i+1; j < string.length; j++) {
+					if(matched.indexOf(j) === -1 && string[i] === string[j]) {
+						matched.push(j);
+					}
+				}
+				result += string[i];
+			}
+		}
 	}
-	return string;
+	return result;
 }
 
-var str = 'sadkjsdnfkjsadfkjbsdfiuhwqef';
+var str = 'sivasivasiva';
 str = removeDuplicateChars(str);
 console.log(str);
