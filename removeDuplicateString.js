26f87c61026a8eddc2aff17fc5be8d3880e9660e
subramaniashiva <vengaishiva@gmail.com>
2015-10-07 22:58:00 +0530

    Added removeDuplicateString file

---
+++ removeDuplicateString.js
@@ -0,0 +1,15 @@
+'use strict';
+/*
+  Problem Statement
+  Remove Duplicate Characters in String
+*/
+function removeDuplicateChars(string) {
+	if(string.length > 1) {
+
+	}
+	return string;
+}
+
+var str = 'sadkjsdnfkjsadfkjbsdfiuhwqef';
+str = removeDuplicateChars(str);
+console.log(str);
