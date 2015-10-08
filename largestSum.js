3025b462d9fd1811dff467e45b61aaebad96b028
subramaniashiva <vengaishiva@gmail.com>
2015-10-08 22:53:40 +0530

    Added largest sum in array stub

---
+++ largestSum.js
@@ -0,0 +1,13 @@
+'use strict';
+/*
+  Problem Statement
+  Find the largest sum of any two elements in an Array
+*/
+function largestSumOfTwo(array) {
+	var result;
+	return result;
+}
+
+var array = [12,13,1,2,45];
+var result = largestSumOfTwo(array);
+console.log(result);
